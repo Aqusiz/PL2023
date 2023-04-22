cc696ba9bc303e72c01947031cf6a8ef3a3ba5ec
Juno <kjo1797@gmail.com>
2023-04-23 04:22:35 +0900

    Fix some check functions and Add sol

--- hw4/worry.ml
+++ hw4/worry.ml
@@ -1,4 +1,5 @@
-open Printf
+(* hw 4-4 of 2017-16140 *)
+(* dumb algorithm: make all cases possible and try *)
 module IntSet = Set.Make(Int)
 type require = id * (cond list)
 and cond = 
@@ -8,9 +9,12 @@ Items of gift list
 | Except of cond * gift list
 and gift = int
 and id = A | B | C | D | E
+(*
 (* helper functions for debug *)
+open Printf
 let value_id id = match id with
 | A -> "A" | B -> "B" | C -> "C" | D -> "D" | E -> "E"
+let value_bool b = if b then "true" else "false"
 let rec value_cond cond = match cond with
 | Items list -> "Items " ^ (String.concat " " (List.map string_of_int list))
 | Same id -> "Same " ^ value_id id
@@ -25,23 +29,26 @@ let value_require_list require_list = String.concat " " (List.map value_require 
 let rec value_shopping_list shopping_list = match shopping_list with
 | [] -> ""
 | (id, list) :: rest -> value_id id ^ " " ^ (String.concat " " (List.map string_of_int list)) ^ " " ^ value_shopping_list rest
-(* compare function for sorting list(asc) *)
-let compare: gift -> gift -> int = fun a b ->
-  if a > b then 1
-  else if a < b then -1
-  else 0
-let rec compare_list: gift list -> gift list -> int = fun l1 l2 ->
-  if (List.length l1) > (List.length l2) then 1
-  else if (List.length l1) < (List.length l2) then -1
-  else (
-    let (a, a_tl) = (List.hd l1, List.tl l1) in
-    let (b, b_tl) = (List.hd l2, List.tl l2) in
+*)
+let shoppingList: require list -> (id * gift list) list = fun require_list ->
+  (* compare function for sorting list(asc) *)
+  let compare: gift -> gift -> int = fun a b ->
     if a > b then 1
     else if a < b then -1
-    else compare_list a_tl b_tl
-  )
-let list_of_set: IntSet.t -> gift list = fun s -> List.of_seq (IntSet.to_seq s)
-let shoppingList: require list -> (id * gift list) list = fun require_list ->
+    else 0
+  in
+  let rec compare_list: gift list -> gift list -> int = fun l1 l2 ->
+    if (List.length l1) > (List.length l2) then 1
+    else if (List.length l1) < (List.length l2) then -1
+    else (
+      let (a, a_tl) = (List.hd l1, List.tl l1) in
+      let (b, b_tl) = (List.hd l2, List.tl l2) in
+      if a > b then 1
+      else if a < b then -1
+      else compare_list a_tl b_tl
+    )
+  in
+  let list_of_set: IntSet.t -> gift list = fun s -> List.of_seq (IntSet.to_seq s) in
   (* make a set of all gifts *)
   let rec make_gift_set: require list -> IntSet.t = fun r_list -> match r_list with
   | [] -> IntSet.empty
@@ -60,7 +67,6 @@ let shoppingList: require list -> (id * gift list) list = fun require_list ->
     IntSet.union (make_gift_set_ cond_list) (make_gift_set r_tl)
   in
   let gifts = List.sort compare (list_of_set (make_gift_set require_list)) in
-  let _ = print_endline (value_gift_list gifts) in
   (* make every possibe gift list (= powerset) *)
   let rec make_all_glist: gift list -> gift list list = fun g_list ->
     match g_list with
@@ -69,24 +75,38 @@ let shoppingList: require list -> (id * gift list) list = fun require_list ->
       let subsets = make_all_glist g_tl in
       subsets @ List.map (fun subset -> g::subset) subsets
   in
-  let gl_list = List.sort compare_list (make_all_glist gifts) in
-  let _ = List.iter (fun gl -> print_string ("[" ^ value_gift_list gl ^ "] ")) gl_list in
+  let make_sorted_gll: gift list -> gift list list = fun gl -> List.sort compare_list (make_all_glist gifts) in
+  let gl_list = (make_sorted_gll gifts) in
   (* functions to check condition *)
+  let rec cond_to_list: (id * gift list) list -> cond -> gift list =
+    fun l c -> match c with
+    | Items g_l -> g_l
+    | Same x -> snd (List.find (fun ig -> (x = fst ig)) l)
+    | Common (c1, c2) -> 
+      let l1 = cond_to_list l c1 in
+      let l2 = cond_to_list l c2 in
+      List.filter (fun x -> List.mem x l2) l1
+    | Except (c', g_l) ->
+      let l1 = cond_to_list l c' in
+      List.filter (fun x -> not (List.mem x g_l)) l1
+  in
   let rec eval_cond: (id * gift list) list -> id -> cond -> bool = fun l x c ->
     let my_gifts = IntSet.of_list (snd (List.find (fun ig -> (fst ig) = x) l)) in
     match c with
     | Items g_list -> IntSet.subset (IntSet.of_list g_list) my_gifts
     | Same id ->
       let target = IntSet.of_list (snd (List.find (fun ig -> (fst ig) = id) l)) in
-      IntSet.subset my_gifts target
-    | Common (c1, c2) -> (eval_cond l x c1) && (eval_cond l x c2)
+      IntSet.subset target my_gifts
+    | Common (c1, c2) -> 
+      let target = IntSet.of_list (cond_to_list l (Common (c1, c2))) in
+      IntSet.subset target my_gifts
     | Except (c, g_list) -> 
       let rec is_in: gift list -> gift list -> bool = fun l1 l2 ->
-        match l2 with
-        | [] -> true
-        | g::tl -> (List.mem g l1) || is_in l1 tl 
+        match l1 with
+        | [] -> false
+        | g::tl -> (List.mem g l2) || is_in tl l2
       in
-      (eval_cond l x c) && (not (is_in (list_of_set my_gifts) g_list))
+      (eval_cond l x c) && (not (is_in g_list (list_of_set my_gifts)))
   in
   let rec eval_cond_list: (id * gift list) list -> id -> cond list -> bool = fun l x c_l ->
     match c_l with
@@ -97,13 +117,34 @@ let shoppingList: require list -> (id * gift list) list = fun require_list ->
   let rec eval: (id * gift list) list -> require list -> bool = fun l r_l ->
     match r_l with
     | [] -> true
-    | r::tl -> (eval_require l r) && (eval l tl) 
+    | r::tl -> (eval_require l r) && (eval l tl)
   in
-  [(A, []);(B, []);(C, []);(D, []);(E, [])]
-
+  (* check every cases and return (id * gift list) list that satisfy all conditions *)
+  let rec sol: id -> gift list list -> require list -> (id * gift list) list -> (id * gift list) list =
+    fun x gll rl acc -> match x, gll with
+    | (_, []) -> [(A, []);(B, []);(C, []);(D, []);(E, [])]
+    | (E, gl::tl) -> 
+      let new_igll = acc @ [(E, gl)] in
+      if eval new_igll rl then new_igll else (sol E tl rl acc)
+    | (D, gl::tl) ->
+      let new_igll = acc @ [(D, gl)] in
+      if (eval (sol E gl_list rl new_igll) rl) then (sol E gl_list rl new_igll) else (sol D tl rl acc)
+    | (C, gl::tl) ->
+      let new_igll = acc @ [(C, gl)] in
+      if (eval (sol D gl_list rl new_igll) rl) then (sol D gl_list rl new_igll) else (sol C tl rl acc)
+    | (B, gl::tl) ->
+      let new_igll = acc @ [(B, gl)] in
+      if (eval (sol C gl_list rl new_igll) rl) then (sol C gl_list rl new_igll) else (sol B tl rl acc)
+    | (A, gl::tl) ->
+      let new_igll = acc @ [(A, gl)] in
+      if (eval (sol B gl_list rl new_igll) rl) then (sol B gl_list rl new_igll) else (sol A tl rl acc)
+    in
+  sol A gl_list require_list []
+(*
 let req_list = 
   [(A, [Items [1;2];Common (Same B, Same C)]);
   (B, [Common (Same C, Items [2;3])]);
   (C, [Items [1];Except (Same A, [3])]);
   (D, []);(E, [])]
-let _ = shoppingList req_list
+let _ = print_endline (value_shopping_list (shoppingList req_list))
+*)
